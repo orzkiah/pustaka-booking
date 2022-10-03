292bbfdc25786e381b11a650139b1032dc0edd82
orzkiah <okehariyu@gmail.com>
2022-10-03 12:28:04 +0700

    Menambahkan semua file

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+    echo "<h1>Perkenalkan Saya Kelompok 1";
+    echo "DIkelompok saya ada Hafid, Oke, Natha, Cila, Desya, Chelsy";
+    }
+}
+
---
+++ application/controllers/latihan1.php
@@ -0,0 +1,20 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+       public function index()
+    {
+        echo "Selamat Datang.. selamat belajar Web Programming";   
+        //$this->load->view('view-latihan1');
+    }
+        public function penj
+        umlahan($n1, $n2)
+    {
+        $this->load->model('model_contoh2');
+        $data['nilai1'] = $n1;  
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $this->model_contoh2->jumlah($n1, $n2);
+
+        $this->load->view('view_latihan1', $data);
+    }
+}
+        
